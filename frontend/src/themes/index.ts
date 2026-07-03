79d887dc7bf316c76a1c8ff46f0aee95118f13b4
Nat <nat.wrw@gmail.com>
2026-07-03 23:08:26 +0700

    feat(frontend): add lavender mist theme

--- frontend/src/themes/index.ts
+++ frontend/src/themes/index.ts
@@ -3,11 +3,13 @@ import { oracleDefault } from './oracle-default';
 import { midnightTeal } from './midnight-teal';
 import { emeraldNoir } from './emerald-noir';
 import { sakura } from './sakura';
+import { lavenderMist } from './lavender-mist';
 
 registerTheme(oracleDefault);
 registerTheme(midnightTeal);
 registerTheme(emeraldNoir);
 registerTheme(sakura);
+registerTheme(lavenderMist);
 
 export { getThemes, getTheme, DEFAULT_THEME_ID } from './registry';
 export type { ThemeDefinition, ThemeTokens } from './types';
